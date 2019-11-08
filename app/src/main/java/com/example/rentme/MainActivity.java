b0c47c640e4ec2e6423ff07d693ea85455d82a5c
Zvi Mints <zvimints@gmail.com>
2019-11-08 12:21:50 +0200

    Added fix grid_view

--- app/src/main/java/com/example/rentme/MainActivity.java
+++ app/src/main/java/com/example/rentme/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.rentme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Main Categories
+     **/
+    GridView gridView;
+    String[] titles = {
+            "קטגוריה 1",
+            "קטגוריה 2",
+            "קטגוריה 3",
+            "קטגוריה 4",
+            "קטגוריה 5",
+            "קטגוריה 6",
+            "קטגוריה 7"};
+
+
+    int[] numberImages = {
+            R.drawable.chairs,
+            R.drawable.electricity,
+            R.drawable.worktools,
+            R.drawable.chairs,
+            R.drawable.electricity,
+            R.drawable.worktools,
+            R.drawable.chairs};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        gridView = findViewById(R.id.grid_view);
+
+        MainAdapter adapter = new MainAdapter(MainActivity.this,titles,numberImages);
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"You Clicked" + titles[+position], Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/rentme/MainAdapter.java
@@ -0,0 +1,55 @@
+package com.example.rentme;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainAdapter extends BaseAdapter  {
+
+    private Context context;
+    private LayoutInflater inflater;
+    private String[] titles;
+    private int[] numberImages;
+
+    public MainAdapter(Context c, String[] titles, int[] numberImages) {
+        this.context = c;
+        this.numberImages = numberImages;
+        this.titles = titles;
+    }
+
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(inflater == null) {
+            inflater = (LayoutInflater) context.getSystemService((Context.LAYOUT_INFLATER_SERVICE));
+        }
+        if(convertView == null) {
+            convertView = inflater.inflate(R.layout.row_item,null);
+        }
+        ImageView imageView = convertView.findViewById(R.id.image_view);
+        TextView textView = convertView.findViewById(R.id.text_view);
+
+        imageView.setImageResource(numberImages[position]);
+        textView.setText(titles[position]);
+        return convertView;
+    }
+}
