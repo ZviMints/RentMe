2d71e03b579d77436a52b1f696b8224fba0f906f
Zvi Mints <zvimints@gmail.com>
2019-11-15 19:51:41 +0200

    Added Last Ads activity

---
+++ app/src/main/java/com/example/rentme/LastAdsActivity.java
@@ -0,0 +1,17 @@
+package com.example.rentme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LastAdsActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.last_ads);
+    }
+}
+
