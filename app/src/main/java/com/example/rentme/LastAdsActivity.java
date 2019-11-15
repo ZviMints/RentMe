1688192ad3b8ea1b30e2ed45b91181f92a4e567e
unknown <eilon26@gmail.com>
2019-11-15 20:47:11 +0200

    list of products

--- app/src/main/java/com/example/rentme/LastAdsActivity.java
+++ app/src/main/java/com/example/rentme/LastAdsActivity.java
@@ -4,14 +4,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class LastAdsActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class LastAdsActivity extends AppCompatActivity {
+    String items[] = new String[] {"apple", "orange","potato","tomato","apple", "orange","potato","tomato","apple", "orange","potato","tomato","apple", "orange","potato","tomato","apple", "orange","potato","tomato"};
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.last_ads);
+
+        ListView listView = (ListView) findViewById(R.id.products_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(adapter);
     }
 }
 
