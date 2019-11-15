1ccab13f79bebe24d8b27e45a7b7a070af2d60c8
Zvi Mints <zvimints@gmail.com>
2019-11-15 20:44:39 +0200

    Added Home Buttons

--- app/src/main/java/com/example/rentme/LastAdsActivity.java
+++ app/src/main/java/com/example/rentme/LastAdsActivity.java
@@ -6,12 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class LastAdsActivity extends Activity {
+    Button homePageBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.last_ads);
+
+        homePageBtn = findViewById((R.id.MainPage));
+        homePageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
