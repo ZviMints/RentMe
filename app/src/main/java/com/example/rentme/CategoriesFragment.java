41b1ce44a287ecc6fc40af961ba30afc8f557a69
unknown <eilon26@gmail.com>
2019-11-19 18:23:10 +0200

    fix jumping activity

---
+++ app/src/main/java/com/example/rentme/CategoriesFragment.java
@@ -0,0 +1,73 @@
+package com.example.rentme;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+
+public class CategoriesFragment extends Fragment {
+
+    private GridView gridView;
+
+
+    String[] titles = {
+            "קטגוריה 1",
+            "קטגוריה 2",
+            "קטגוריה 3",
+            "קטגוריה 4",
+            "קטגוריה 5",
+            "קטגוריה 6",
+            "קטגוריה 7",
+            "Checking Exception"};
+
+
+    int[] numberImages = {
+            R.drawable.vacation,
+            R.drawable.sport,
+            R.drawable.party,
+            R.drawable.chairs,
+            R.drawable.electricity,
+            R.drawable.worktools,
+            R.drawable.chairs};
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_categories, container, false);
+
+        gridView = view.findViewById(R.id.grid_view);
+
+        MainAdapter adapter = new MainAdapter(getContext(),titles,numberImages);
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(),"You Clicked" + titles[+position], Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+}
