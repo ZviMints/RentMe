b22947de7ea89388a16b18688a707fb381698940
Zvi Mints <zvimints@gmail.com>
2019-11-22 20:45:40 +0200

    Add More UI To InItemActivity

--- app/src/main/java/com/example/rentme/InItemActivity.java
+++ app/src/main/java/com/example/rentme/InItemActivity.java
@@ -3,8 +3,10 @@ package com.example.rentme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class InItemActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
